d2fe86185b1ca8ede1ce3ba3ed8db9a380c9e19b
GitHub Action <action@github.com>
2026-09-09 22:40:30 +0800

    2026-09-09 22:40

--- Kazumi/lib/pages/about/about_module.dart
+++ Kazumi/lib/pages/about/about_module.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:kazumi/pages/about/about_page.dart';
 import 'package:kazumi/pages/about/credits_page.dart';
 import 'package:kazumi/pages/my/my_controller.dart';
-import 'package:kazumi/request/config/api_endpoints.dart';
 
 final aboutModule = createModule(
   path: '/about',
@@ -15,14 +13,6 @@ final aboutModule = createModule(
           onCheckUpdate: inject<MyController>().checkUpdate,
         ),
       )
-      ..route('/credits', child: (context, state) => const CreditsPage())
-      ..route(
-        '/license',
-        child: (context, state) => const LicensePage(
-          applicationName: 'Kazumi',
-          applicationVersion: ApiEndpoints.version,
-          applicationLegalese: 'Kazumi · GNU General Public License v3.0',
-        ),
-      );
+      ..route('/credits', child: (context, state) => const CreditsPage());
   },
 );
--- Kazumi/lib/pages/settings/settings_module.dart
+++ Kazumi/lib/pages/settings/settings_module.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:kazumi/pages/about/about_module.dart';
 import 'package:kazumi/pages/download/download_page_module.dart';
@@ -22,59 +23,79 @@ import 'package:kazumi/pages/settings/sync/webdav_server_page.dart';
 import 'package:kazumi/pages/settings/sync/webdav_sync_page.dart';
 import 'package:kazumi/pages/settings/theme_settings_page.dart';
 import 'package:kazumi/pages/settings/update_settings.dart';
+import 'package:kazumi/request/config/api_endpoints.dart';
 
 final settingsModule = createModule(
   path: '/settings',
   register: (c) {
+    c.route(
+      '/',
+      child: (context, state) => SettingsPage(location: state.uri.path),
+      children: (sub) {
+        sub
+          ..route('/', child: (context, state) => const SettingsIndexPage())
+          ..route('/sync', child: (context, state) => const SyncSettingsPage())
+          ..route('/bangumi/',
+              child: (context, state) => const BangumiSyncPage())
+          ..route('/webdav/', child: (context, state) => const WebDavSyncPage())
+          ..route('/webdav/editor',
+              child: (context, state) => const WebDavServerPage())
+          ..route(
+            '/update',
+            child: (context, state) => const UpdateSettingsPage(),
+          )
+          ..route('/storage',
+              child: (context, state) => const StorageSettingsPage())
+          ..route('/storage/logs', child: (context, state) => const LogsPage())
+          ..route('/theme',
+              child: (context, state) => const ThemeSettingsPage())
+          ..route(
+            '/theme/display',
+            child: (context, state) => const SetDisplayMode(),
+          )
+          ..route(
+            '/keyboard',
+            child: (context, state) => const KeyboardSettingsPage(),
+          )
+          ..route('/player',
+              child: (context, state) => const PlayerSettingsPage())
+          ..route(
+            '/player/decoder',
+            child: (context, state) => const DecoderSettings(),
+          )
+          ..route(
+            '/player/renderer',
+            child: (context, state) => const RendererSettings(),
+          )
+          ..route(
+            '/interface',
+            child: (context, state) => const InterfaceSettingsPage(),
+          )
+          ..module(proxyModule)
+          ..route(
+            '/player/super',
+            child: (context, state) => const SuperResolutionSettings(),
+          )
+          ..module(aboutModule)
+          ..module(pluginModule)
+          ..module(danmakuModule)
+          ..route(
+            '/download-settings',
+            child: (context, state) => const DownloadSettingsPage(),
+          );
+      },
+    );
     c
-      ..route('/', child: (context, state) => const SettingsPage())
-      ..route('/sync', child: (context, state) => const SyncSettingsPage())
-      ..route('/bangumi/', child: (context, state) => const BangumiSyncPage())
-      ..route('/webdav/', child: (context, state) => const WebDavSyncPage())
-      ..route('/webdav/editor',
-          child: (context, state) => const WebDavServerPage())
+      // Root route: the license browser must cover the settings shell.
       ..route(
-        '/update',
-        child: (context, state) => const UpdateSettingsPage(),
+        '/about/license',
+        child: (context, state) => const LicensePage(
+          applicationName: 'Kazumi',
+          applicationVersion: ApiEndpoints.version,
+          applicationLegalese: 'Kazumi · GNU General Public License v3.0',
+        ),
       )
-      ..route('/storage',
-          child: (context, state) => const StorageSettingsPage())
-      ..route('/storage/logs', child: (context, state) => const LogsPage())
-      ..route('/theme', child: (context, state) => const ThemeSettingsPage())
-      ..route(
-        '/theme/display',
-        child: (context, state) => const SetDisplayMode(),
-      )
-      ..route(
-        '/keyboard',
-        child: (context, state) => const KeyboardSettingsPage(),
-      )
-      ..route('/player', child: (context, state) => const PlayerSettingsPage())
-      ..route(
-        '/player/decoder',
-        child: (context, state) => const DecoderSettings(),
-      )
-      ..route(
-        '/player/renderer',
-        child: (context, state) => const RendererSettings(),
-      )
-      ..route(
-        '/interface',
-        child: (context, state) => const InterfaceSettingsPage(),
-      )
-      ..module(proxyModule)
-      ..route(
-        '/player/super',
-        child: (context, state) => const SuperResolutionSettings(),
-      )
-      ..module(aboutModule)
-      ..module(pluginModule)
       ..module(historyModule)
-      ..module(danmakuModule)
-      ..module(downloadModule)
-      ..route(
-        '/download-settings',
-        child: (context, state) => const DownloadSettingsPage(),
-      );
+      ..module(downloadModule);
   },
 );
